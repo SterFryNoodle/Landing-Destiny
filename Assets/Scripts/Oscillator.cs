dff4ac71cc724359ae27283bc83c3e6102134de2
SterFryNoodle <revangcruz@yahoo.com>
2024-01-31 00:16:19 -0800

    Created manual oscillator(temp) from script

--- Assets/Scripts/Oscillator.cs
+++ Assets/Scripts/Oscillator.cs
@@ -6,16 +6,18 @@ public class Oscillator : MonoBehaviour
 {
     Vector3 startingPos;
     [SerializeField] Vector3 movementVector;
-    [SerializeField] Vector3 movementFactor;
+    [SerializeField][Range(0,1)] float movementFactor; //the range class allows to turn a variable into a slider between
+                                                       // a min and max range in the inspector.
     
     void Start()
     {
-        
+        startingPos = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 offset = movementVector * movementFactor;
+        transform.position = startingPos + offset;
     }
 }
