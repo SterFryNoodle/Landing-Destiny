592c41f5910a97fe8c2976bffa8c9599d4d23658
SterFryNoodle <revangcruz@yahoo.com>
2024-01-31 12:39:42 -0800

    Added comment for oscillator script

--- Assets/Scripts/Oscillator.cs
+++ Assets/Scripts/Oscillator.cs
@@ -6,7 +6,7 @@ public class Oscillator : MonoBehaviour
 {
     Vector3 startingPos;
     [SerializeField] Vector3 movementVector;
-    [SerializeField][Range(0,1)] float movementFactor; //the range class allows to turn a variable into a slider between
+    [SerializeField] float movementFactor; //the range class allows to turn a variable into a slider between
                                                        // a min and max range in the inspector.
     [SerializeField] float period = 2f;
 
@@ -18,12 +18,12 @@ public class Oscillator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float cycles = Time.time / period; 
+        float cycles = Time.time / period; // continually grows over time.
         const float tau = Mathf.PI * 2; // equation to get 2pi.
         
-        float rawSinWave = Mathf.Sin(cycles * tau);
+        float rawSinWave = Mathf.Sin(cycles * tau); // going from -1 to 1
 
-        movementFactor = (rawSinWave + 1f) / 2f;
+        movementFactor = (rawSinWave + 1f) / 2f; // adjusts values of sin wave to 0 to 1 for object movement.
         Vector3 offset = movementVector * movementFactor;
         transform.position = startingPos + offset;
     }
